442c2c8164006785bbd8c933c7fdd5148ab34e31
randomco'er <lemmonboys99@gmail.com>
2026-04-07 20:38:53 -0700

    added offboard script (still working on it and I forgot to update in awhile)

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from mavsdk import System
+from mavsdk.offboard import PositionNedYaw
 import asyncio
 
 drone = System()
-task_list = []
 async def main(): 
 
     await drone.connect('udpin://0.0.0.0:14540')
@@ -24,16 +24,27 @@ async def main():
             continue 
 
     await drone.action.arm() 
-    await drone.action.takeoff() 
 
     async for checks in drone.telemetry.in_air():
         if checks:
             print("drone is in air, starting countdown", checks)
-            batteryd = asyncio.create_task(battery()) 
-            coordinatesd = asyncio.create_task(coordinates())
-            task_list = [batteryd, coordinatesd]
-
-            await asyncio.sleep(30)
+            await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, 0.0))
+            await drone.offboard.start()
+            async for air in drone.telemetry.in_air():
+                if air:
+                    await drone.action.takeoff() 
+                    await asyncio.sleep(5)
+                    break
+                else:
+                    continue 
+        
+            
+            async for coords in drone.telemetry.position_velocity_ned():
+                await drone.offboard.set_position_ned(PositionNedYaw(0.0, -5.0, -1* coords.position.down_m, 0.0))
+                if -5.1 < coords.position.east_m < -4.9:
+                    break
+                else:
+                    continue
             break
         else:
             print("drone is not in air, can not start countdown", checks)
@@ -47,7 +58,7 @@ async def main():
         else:
             break 
 
-    await drone.action.disarm()
+    print('disarm')
 
 if __name__ == "__main__":
     asyncio.run(main()) 
